41603cb639973049cb139cc12331dc9475afc8a6
Jose Lumbantobing <jolbntobing2@gmail.com>
2023-04-13 00:36:35 +0700

    Create Skills component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import '@component/styles/globals.css'
 import '../component/header/header.css'
 import '../component/home/home.css'
 import '../component/about/about.css'
+import '../component/skills/skills.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import { Header } from '@component/component/header/header'
 import { Base } from '@component/component/home/Home'
 import { About } from '@component/component/about/About'
+import { Skills } from '@component/component/skills/Skills'
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <main className='main'>
         <Base />
         <About />
+        <Skills />
       </main>
     </>
   )
