25436e3066234dc72b92f863612315128741517b
Jose Lumbantobing <jolbntobing2@gmail.com>
2023-04-14 00:24:18 +0700

    Create responsive Skills components

---
+++ src/component/skills/Frontend.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+
+export const Frontend = () => {
+  return (
+    <div className="skills__content">
+        <h3 className="skills__title">Frontend Developer</h3>
+        <div className="skills__box">
+            <div className="skills__group">
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">HTML</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">CSS</h3>
+                        <span className="skills__level">Upper Intermediate</span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">Javascript</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">React JS</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">Next JS</h3>
+                        <span className="skills__level">Elementary</span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">Typescript</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">Bootstrap</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">Jest</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">Git & Github</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">Figma</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
--- src/component/skills/Skills.jsx
+++ src/component/skills/Skills.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Frontend } from './Frontend'
 
 export const Skills = () => {
   return (
-    <div>Skills</div>
+    <section className="skills section" id='skills'>
+      <h2 className="section__title">Skills</h2>
+      <span className="section__subtitle">My Technical Level</span>
+
+      <div className="skill__container container grid">
+        <Frontend />
+      </div>
+    </section>
   )
 }
