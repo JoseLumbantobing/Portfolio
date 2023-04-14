cef9c77f84b2bcd152ae4a107b0660c44a514ac3
Jose Lumbantobing <jolbntobing2@gmail.com>
2023-04-14 21:24:21 +0700

    Make Portfolio components responsive and done

---
+++ src/component/portfolio/Data.jsx
@@ -0,0 +1,45 @@
+import work1 from "../../assets/Movie.jpg"
+import Work2 from "../../assets/Cart.jpg"
+import Work3 from "../../assets/Joke.jpg"
+import Work4 from "../../assets/Dictionary.jpg"
+import Work5 from "../../assets/Weather.jpg"
+import Work6 from "../../assets/ToDo.jpg"
+
+export const projectsData = [
+    {
+        id: 1,
+        image: work1,
+        title: 'Movie App',
+        url: 'https://mondo-movie.vercel.app/',
+    },
+    {
+        id: 2,
+        image: Work2,
+        title: 'Shopping Cart App',
+        url: 'https://shopping-cart-m-plaza.vercel.app/',
+    },
+    {
+        id: 3,
+        image: Work3,
+        title: 'Random Joke App',
+        url: 'https://get-random-joke.vercel.app/',
+    },
+    {
+        id: 4,
+        image: Work4,
+        title: 'Dictionary App',
+        url: 'https://dictionary-app-psi.vercel.app/',
+    },
+    {
+        id: 5,
+        image: Work5,
+        title: 'Weather App',
+        url: 'https://weather-on.vercel.app/',
+    },
+    {
+        id: 6,
+        image: Work6,
+        title: 'To Do App',
+        url: 'https://list-for-todo.vercel.app/',
+    },
+];
--- src/component/portfolio/Portfolio.jsx
+++ src/component/portfolio/Portfolio.jsx
@@ -1,7 +1,49 @@
 import React from 'react'
+import Works from './Works'
+import { projectsData } from './Data'
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper";
 
 export const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section className="work container section" id='portfolio'>
+      <h2 className="section__title">Portfolio</h2>
+      <span className="section__subtitle">Most Recent Works</span>
+      
+      <Swiper className="work__container container grid"
+        loop={true}
+        grabCursor={true}
+        spaceBetween={24}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          576: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+        }}
+        modules={[Pagination]}
+      >
+        {projectsData.map(item => {
+          return (
+            <SwiperSlide>
+              <Works item={item} key={item.id} />
+            </SwiperSlide>
+        )})}
+      </Swiper>
+    </section>
   )
 }
